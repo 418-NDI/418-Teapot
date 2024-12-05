bb4199584daac27bb3011fbb1880191b183745ce
mvespier <vespiermichel@gmail.com>
2024-12-05 23:17:22 +0100

    static qui fonctionne

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, abort
 import json
 
 def get_data():
--- freeze.py
+++ freeze.py
@@ -3,5 +3,11 @@ from app import app
 
 freezer = Freezer(app)
 
+# @freezer.register_generator
+# def corps_details():
+#     for partie in app:
+#         yield {'product_id': product.id}
+
+
 if __name__=='__main__':
     freezer.freeze()
