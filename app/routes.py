19bf40fba21eb751c48b6778b5ef56c983bfc4f2
mvespier <vespiermichel@gmail.com>
2024-12-05 20:34:36 +0100

    build

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,14 @@ from app import app
 from flask import render_template
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template("index.html")
 
-@app.route('/podcast')
+@app.route('/podcast.html')
 def podcast():
     return render_template("podcast.html")
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template("about.html")
