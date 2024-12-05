2027eac35d52437ddcd1f6c8f6b4e6813fc6f794
mvespier <vespiermichel@gmail.com>
2024-12-05 20:25:29 +0100

    added About and Podcast

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/podcast')
+def podcast():
+    return render_template("podcast.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
