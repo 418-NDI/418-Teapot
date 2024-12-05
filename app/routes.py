de458bb27654378d8e24793ddf30dff5a27052f1
mvespier <vespiermichel@gmail.com>
2024-12-05 22:45:20 +0100

    corps

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,25 @@
 from app import app
 from flask import render_template
+import json
+
+def get_data():
+    with open('app/corps.json') as file:
+        data = json.load(file)
+    return {d['title'] : d['content'] for d in data}
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", collection=get_data().keys())
 
 @app.route('/podcast.html')
 def podcast():
-    return render_template("podcast.html")
+    return render_template("podcast.html", collection=get_data().keys())
 
 @app.route('/about.html')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", collection=get_data().keys())
+
+@app.route('/<partie>.html')
+def corps(partie):
+    return render_template("corps.html", element=partie, desc = get_data()[partie], collection=get_data().keys())
