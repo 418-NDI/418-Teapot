76e575938ab991a0a6e503baf5340350926cf9f9
mvespier <vespiermichel@gmail.com>
2024-12-06 05:16:40 +0100

    css button

---
+++ app/build/static/scriptSelection.js
@@ -0,0 +1,32 @@
+function animSelectionEntreeSouris(event){
+	setOpaciteImages(event.target.className);
+}
+
+function animSelectionSortieSouris(event){
+	resetOpaciteImages();
+}
+
+function setOpaciteImages(classeSelectionnee){
+	elements = document.getElementsByClassName("image-corps");
+	console.log(elements.length);
+	for(const element of elements)
+	{
+		console.log(element.id);
+		if(!element.classList.contains(classeSelectionnee))
+		{
+			element.style["filter"] = "saturate(0%)";
+		}
+		else
+		{
+			element.style["filter"] = "saturate(150%)";
+		}
+	}
+}
+
+function resetOpaciteImages(){
+	elements = document.getElementsByClassName("image-corps");
+	for(const element of elements)
+	{
+		element.style["filter"] = "saturate(100%)";
+	}
+}
